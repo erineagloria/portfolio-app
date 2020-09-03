4b1d9334decd6fb43be5381557b2c53d94c0ed34
Erinea Gloria <erineagloria@gmail.com>
2020-09-04 09:57:42 +1000

    added box shadowing and other effects

--- src/Introduction.js
+++ src/Introduction.js
@@ -10,12 +10,11 @@ export default class Introduction extends Component {
                     <br/>
                     <br/>
                     <h4 className="welcome-en">Welcome</h4><h4 className="welcome-fr">Bienvenue //</h4>
-                    <h1 className="name">Hi, I'm Erinea</h1>
-                    {/* <span className="phonetics">/air-a-ney-yah/</span> */}
-                    <h3>Software Developer</h3>
+                    <h1 className="name">Hi, <br/> I'm Erinea Gloria</h1>
+                    {/* <h3>Software Developer</h3> */}
 
-                    <p>A Sales and Events Manager turned Software Developer</p>
-                    <p><a href="mailto:hello@erineagloria.com"><span className="material-icons md-36 email">email</span></a> Find me on <a href="https://www.linkedin.com/in/erinea-gloria/" target="_blank" className="links" rel="noopener noreferrer" >LinkedIn</a>, <a href="https://github.com/erineagloria" target="_blank" className="links" rel="noopener noreferrer">Github</a> and <a href="https://medium.com/@erineagloria" target="_blank" className="links" rel="noopener noreferrer">Medium</a></p>
+                    <p>...a Sales and Events Manager turned Software Developer</p>
+                    <p><a href="mailto:hello@erineagloria.com"><span className="material-icons md-36 email">email</span></a>    Find me on <a href="https://www.linkedin.com/in/erinea-gloria/" target="_blank" className="links" rel="noopener noreferrer" >LinkedIn</a>, <a href="https://github.com/erineagloria" target="_blank" className="links" rel="noopener noreferrer">Github</a> and <a href="https://medium.com/@erineagloria" target="_blank" className="links" rel="noopener noreferrer">Medium</a></p>
 
                 </div>
             </div>
