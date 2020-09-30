3ff19aa39e2a9e328307680557ed768983257bf5
Erinea Gloria <erineagloria@gmail.com>
2020-09-30 14:16:49 +1000

    changed to intro page

--- src/Introduction.js
+++ src/Introduction.js
@@ -16,7 +16,6 @@ export default class Introduction extends Component {
                             <img className="icon" src="https://pngimg.com/uploads/ruby/ruby_PNG28.png" alt="ruby"/>
                             <img className="icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/600px-JavaScript-logo.png" alt="javascript"/>
                             <img className="icon" src="https://www.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" alt="react"/>
-                            <img className="icon" src="https://lh3.googleusercontent.com/proxy/bEuDJ05rnZAaJyDTOVAVGBq0b2i5Cd71ZM4MvrJPff5afuZNrbrz4zMIuMQtv0U3ggg4Jr8MnYcKgyNW4ResxKpOnrmjAbMzzZ1KxdbcoOjyGi1b2X1XRyPp6jWc8llULZrI1pAA2hnBOQ" alt="python"/>
                     </div>
                 </div>
                 <img className="profile-photo" src="https://github.com/erineagloria/portfolio-app/blob/master/images/photo.png?raw=true" alt="profile"/>
